999254591a91b80a34240b68c50cfa0845936bed
Ewan Brinkman <ewanbrinkman@gmail.com>
2023-07-01 14:23:50 -0700

    feat: load in location data

--- src/location.py
+++ src/location.py
@@ -4,9 +4,11 @@ class Location:
         self.map_x = map_x
         self.map_y = map_y
 
-    @classmethod
-    def from_filename(cls, filename):
-        pass
+    def get_position(self):
+        return self.map_x, self.map_y
 
     def draw(self, window):
         pass
+
+    def __str__(self):
+        return f"Location with image {self.image} at ({self.map_x}, {self.map_y})"
